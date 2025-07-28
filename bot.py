11a60fad9a98e25af1876c449759b8848cf514c0
reptyd <81038781+reptyd@users.noreply.github.com>
2025-07-29 02:48:02 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,17 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
 from config import TOKEN
 from handlers import user, operator
-from aiogram.client.default import DefaultBotProperties
-from aiogram.types import BotCommand
 
-bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
-
 async def set_bot_commands():
     await bot.set_my_commands([
         BotCommand(command="start", description="Начать обращение"),
         BotCommand(command="help", description="Как пользоваться ботом")
     ])
-
 async def main():
-    await set_bot_commands()
     dp.include_router(user.router)
     dp.include_router(operator.router)
     await dp.start_polling(bot)
