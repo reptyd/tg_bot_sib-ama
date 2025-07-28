ef7d7bb4027abe9e49a2a768ead81e613e6c62f3
reptyd <81038781+reptyd@users.noreply.github.com>
2025-07-28 13:14:27 -0700

    Add files via upload

---
+++ support_bot1/bot.py
@@ -0,0 +1,16 @@
+
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import TOKEN
+from handlers import user, operator
+from aiogram.client.default import DefaultBotProperties
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+dp = Dispatcher()
+
+async def main():
+    dp.include_router(user.router)
+    dp.include_router(operator.router)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
