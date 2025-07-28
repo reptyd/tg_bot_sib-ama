89e11805c1eef01c54c496abe48145787490b5bb
reptyd <81038781+reptyd@users.noreply.github.com>
2025-07-29 01:46:22 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from handlers import user, operator
+from config import BOT_TOKEN
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
 
